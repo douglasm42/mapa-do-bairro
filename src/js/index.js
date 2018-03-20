da051b88c32d180faae9416db92e66d47ea289b8
Douglas de Freitas <douglasmfreitas@gmail.com>
2018-03-19 21:03:40 -0300

    Adiciona o container do menu lateral

--- src/js/index.js
+++ src/js/index.js
@@ -17,6 +17,30 @@ export default class Hello extends Component {
 	}
 }
 
+function showSidebar() {
+	document.getElementById('sidebar').style.left = 0;
+}
+
+function hideSidebar() {
+	let sidebar = document.getElementById('sidebar');
+	sidebar.style.left = -sidebar.style.width;
+}
+
+function toggleSidebar() {
+	let sidebar = document.getElementById('sidebar');
+	console.log(sidebar.style.left);
+	if(sidebar.offsetLeft != 0) {
+		sidebar.style.left = 0;
+		console.log("Mostrando sidebar!");
+	} else {
+		sidebar.style.left = (-sidebar.offsetWidth)+'px';
+		console.log("Escondendo sidebar!");
+		console.log(-sidebar.offsetWidth);
+	}
+}
+
+window.toggleSidebar = toggleSidebar;
+
 // ReactDOM.render(<Hello/>, document.getElementById('app'));
 
 // loadGoogleMapsAPI();
