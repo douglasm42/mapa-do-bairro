646676739bc206b34eff009ac55e0e936129b20a
Douglas de Freitas <douglasmfreitas@gmail.com>
2018-03-23 18:43:19 -0300

    Mudando a indentação para dois espaços

--- src/js/index.js
+++ src/js/index.js
@@ -8,35 +8,35 @@ import '../css/main.scss'
 console.log("Hello World!");
 
 export default class Hello extends Component {
-	render() {
-		return (
-			<div>
-				<div id='map'></div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <div id='map'></div>
+      </div>
+    );
+  }
 }
 
 function showSidebar() {
-	document.getElementById('sidebar').style.left = 0;
+  document.getElementById('sidebar').style.left = 0;
 }
 
 function hideSidebar() {
-	let sidebar = document.getElementById('sidebar');
-	sidebar.style.left = -sidebar.style.width;
+  let sidebar = document.getElementById('sidebar');
+  sidebar.style.left = -sidebar.style.width;
 }
 
 function toggleSidebar() {
-	let sidebar = document.getElementById('sidebar');
-	console.log(sidebar.style.left);
-	if(sidebar.offsetLeft != 0) {
-		sidebar.style.left = 0;
-		console.log("Mostrando sidebar!");
-	} else {
-		sidebar.style.left = (-sidebar.offsetWidth)+'px';
-		console.log("Escondendo sidebar!");
-		console.log(-sidebar.offsetWidth);
-	}
+  let sidebar = document.getElementById('sidebar');
+  console.log(sidebar.style.left);
+  if(sidebar.offsetLeft != 0) {
+    sidebar.style.left = 0;
+    console.log("Mostrando sidebar!");
+  } else {
+    sidebar.style.left = (-sidebar.offsetWidth)+'px';
+    console.log("Escondendo sidebar!");
+    console.log(-sidebar.offsetWidth);
+  }
 }
 
 window.toggleSidebar = toggleSidebar;
--- src/js/map.js
+++ src/js/map.js
@@ -1,75 +1,75 @@
 export default class Map {
-	constructor(center, zoom) {
-		this.map = new google.maps.Map(document.getElementById('map'), {
-			center: point,
-			zoom: 14
-		});
-	}
+  constructor(center, zoom) {
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      center: point,
+      zoom: 14
+    });
+  }
 
-	addMarker(marker) {
-	}
+  addMarker(marker) {
+  }
 }
 
 var APIKey = 'AIzaSyA7oz8Q4iD0yb1Qkokep8DAz78j27XjpfQ';
 
 function loadGoogleMapsAPI() {
-	const script = document.createElement("script");
-	script.src = `https://maps.googleapis.com/maps/api/js?key=${APIKey}&callback=initMap&libraries=places`;
-	script.async = true;
-	script.defer = true;
-	document.body.appendChild(script);
+  const script = document.createElement("script");
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${APIKey}&callback=initMap&libraries=places`;
+  script.async = true;
+  script.defer = true;
+  document.body.appendChild(script);
 }
 
 function initMap() {
-	// Create a map object and specify the DOM element for display.
-	var goiania = { lat: -16.680650, lng: -49.256318 };
+  // Create a map object and specify the DOM element for display.
+  var goiania = { lat: -16.680650, lng: -49.256318 };
 
-	var map = new google.maps.Map(document.getElementById('map'), {
-		center: goiania,
-		zoom: 14
-	});
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: goiania,
+    zoom: 14
+  });
 
-	var service = new google.maps.places.PlacesService(map);
-	var request = {
-		location: goiania,
-		radius: '500',
-		types: ['store']
-	};
+  var service = new google.maps.places.PlacesService(map);
+  var request = {
+    location: goiania,
+    radius: '500',
+    types: ['store']
+  };
 
-	function createMarkers(places) {
-		var bounds = new google.maps.LatLngBounds();
-		var placesList = document.getElementById('places');
+  function createMarkers(places) {
+    var bounds = new google.maps.LatLngBounds();
+    var placesList = document.getElementById('places');
 
-		for (var i = 0, place; place = places[i]; i++) {
-			var image = {
-				url: place.icon,
-				size: new google.maps.Size(71, 71),
-				origin: new google.maps.Point(0, 0),
-				anchor: new google.maps.Point(17, 34),
-				scaledSize: new google.maps.Size(25, 25)
-			};
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
 
-			var marker = new google.maps.Marker({
-				map: map,
-				//icon: image,
-				title: place.name,
-				position: place.geometry.location
-			});
+      var marker = new google.maps.Marker({
+        map: map,
+        //icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
 
-			placesList.innerHTML += '<li>' + place.name + '</li>';
+      placesList.innerHTML += '<li>' + place.name + '</li>';
 
-			bounds.extend(place.geometry.location);
-		}
-		map.fitBounds(bounds);
-	}
+      bounds.extend(place.geometry.location);
+    }
+    map.fitBounds(bounds);
+  }
 
-	function searchCallback(results, status) {
-		if (status == google.maps.places.PlacesServiceStatus.OK) {
-			createMarkers(results);
-		}
-	}
+  function searchCallback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      createMarkers(results);
+    }
+  }
 
-	service.nearbySearch(request, searchCallback);
+  service.nearbySearch(request, searchCallback);
 }
 
 window.initMap = initMap;
